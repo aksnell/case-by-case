851f4839fac5d49057ec0f4b09492950ee2afd47
Alexander Snell <alexksnell@gmail.com>
2020-07-25 20:32:20 -0400

    Feature: Integrated initial mapbox component

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,62 +1,40 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import ReactMapboxGl from 'react-mapbox-gl'
+import mapboxgl from 'mapbox-gl'
+
+
 
 export function Home() {
+
+  const mapContainer = useRef(null);
+
+  const [ lng, setLng ] = useState(5)
+  const [ lat, setLat ] = useState(34)
+  const [ zoom, setZoom ] = useState(1.5)
+
+  const Map = ReactMapboxGl({
+    accessToken: 'pk.eyJ1IjoiYWtzbmVsbCIsImEiOiJja2QyNWIxaGwwMHF5MnNtb2hlM3kzODc5In0._iaqzpg_V-an3ngDTSWfmQ'
+  })
+
+  // useEffect(() => {
+  //   const mapBox = new mapboxgl.Map({
+  //     container: mapContainer.current,
+  //     style: 'mapbox://styles/aksnell/ckd27ujkt1un91iqouslqh4sk',
+  //   })
+  // })
+
   return (
-    <div>
-      <h1>Hello Student!</h1>
-      <p>Welcome to your new SDG SPA, built with:</p>
-      <ul>
-        <li>
-          <a href="https://get.asp.net/">ASP.NET Core</a> and{' '}
-          <a href="https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx">
-            C#
-          </a>{' '}
-          for cross-platform server-side code
-        </li>
-        <li>
-          <a href="https://facebook.github.io/react/">React</a> for client-side
-          code
-        </li>
-        <li>
-          <a href="https://www.typescriptlang.org/">Typescript</a> for better
-          client-side code
-        </li>
-        <li>
-          <a href="https://sass-lang.com/">Sass</a> for better css
-        </li>
-        <li>
-          <a href="http://getbootstrap.com/">Bootstrap</a> for layout and
-          styling
-        </li>
-      </ul>
-      <p>To help you get started, we have also set up:</p>
-      <ul>
-        <li>
-          <strong>Client-side navigation</strong>.
-        </li>
-        <li>
-          <strong>Development server integration</strong>. In development mode,
-          the development server from <code>create-react-app</code> runs in the
-          background automatically, so your client-side resources are
-          dynamically built on demand and the page refreshes when you modify any
-          file.
-        </li>
-        <li>
-          <strong>Efficient production builds</strong>. In production mode,
-          development-time features are disabled, and your{' '}
-          <code>dotnet publish</code> configuration produces minified,
-          efficiently bundled JavaScript files.
-        </li>
-        <li>
-          <strong>Docker</strong>.
-        </li>
-      </ul>
-      <p>
-        The <code>ClientApp</code> subdirectory is a standard React application
-        based on the <code>create-react-app</code> template. If you open a
-        command prompt in that directory, you can run <code>npm</code> commands
-        such as <code>npm test</code> or <code>npm install</code>.
-      </p>
-    </div>
+    <Map
+      style="mapbox://styles/aksnell/ckd27ujkt1un91iqouslqh4sk"
+      center={[-82.460258, 27.963515]}
+      zoom={[16]}
+      bearing={[-20.00]}
+      pitch={[52.50]}
+      containerStyle={{
+        height: '100vh',
+        width: '100vh'
+      }}
+    >
+    </Map>
   )
 }
