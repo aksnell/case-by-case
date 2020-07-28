a05a7046e4de5f5ade46caf27818478da746e8a8
Alexander Snell <alexksnell@gmail.com>
2020-07-28 11:33:45 -0400

    Refactor: change from using react Mapbox library to standard mapbox library

--- ClientApp/src/pages/Home.jsx
+++ ClientApp/src/pages/Home.jsx
@@ -1,66 +1,73 @@
-import React from 'react'
-import ReactMapboxGl, { GeoJSONLayer } from 'react-mapbox-gl'
+import React, { useEffect, useState, useRef } from 'react'
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+// const json = require("./test.json")
 
-const json = require("./test.json")
+// const convert = (json) => {
+//   const shelters = {
+//     type: "FeatureCollection",
+//     features: []
+//   }
+//   for (let loc of json.filter(loc => loc["is_geocoded"])) {
+//     console.log(loc["name"])
+//     shelters["features"].push({
+//       type: "Feature",
+//       geometry: {
+//         type: "Point",
+//         coordinates: [
+//           loc["longitude"],
+//           loc["latitude"]
+//         ]
+//       },
+//       properties: {
+//         "id": loc["id"],
+//         "phone": loc["phone"],
+//         "place": loc["name"]
+//       }
+//     })
+//   }
+//   return shelters
+// }
 
-const convert = (json) => {
-  const shelters = {
-    type: "FeatureCollection",
-    features: []
-  }
-  for (let loc of json.filter(loc => loc["is_geocoded"])) {
-    shelters["features"].push({
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [
-          loc["longitude"],
-          loc["latitude"]
-        ]
-      },
-      properties: {
-        "id": loc["id"],
-        "phone": loc["phone"],
-        "place": loc["name"]
-      }
-    })
-  }
-  return shelters
+// const shelterGeoJson = convert(json)
+
+const mapStyle = {
+  width: "100vw",
+  height: "100vh",
+  position:" absolute"
 }
 
-const shelterGeoJson = convert(json)
 
 export function Home() {
+  const [ map, setMap ] = useState(null)
+  const mapContainer = useRef(null)
 
-  const Map = ReactMapboxGl({
-    accessToken: 'pk.eyJ1IjoiYWtzbmVsbCIsImEiOiJja2QyNWIxaGwwMHF5MnNtb2hlM3kzODc5In0._iaqzpg_V-an3ngDTSWfmQ'
-  })
+
+  // Map initialiization
+  useEffect(() => {
+    mapboxgl.accessToken =  'pk.eyJ1IjoiYWtzbmVsbCIsImEiOiJja2Q2MzcxYmgwdjNkMzFvZGhqdmxrYmpvIn0.-c3cS4I9ZS4TVtCltd5Q8Q'
+    const initializeMap = ({ setMap, mapContainer}) => {
+      const map = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [-82.460258, 27.963515],
+        zoom: 16,
+        bearing: -20.00,
+        pitch: 52.50
+      })
+
+      map.on("load", () => {
+        setMap(map)
+        map.resize()
+      })
+    }
+
+    if (!map) initializeMap({ setMap, mapContainer })
+
+  }, [map])
 
   return (
-    <Map
-      style="mapbox://styles/aksnell/ckd27ujkt1un91iqouslqh4sk"
-      center={[-82.460258, 27.963515]}
-      zoom={[16]}
-      bearing={[-20.00]}
-      pitch={[52.50]}
-      containerStyle={{
-        height: '100vh',
-        width: '100vh'
-      }}
-    >
-    <GeoJSONLayer
-      data={shelterGeoJson}
-      circleLayout={{
-        "visibility": "visible"
-      }}
-      symbolLayout={{
-        "text-field": "{place}",
-        "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-        "text-offset": [0, 0.6],
-        "text-anchor": "top"
-      }}
-    />
-    </Map>
+    <div ref={el => (mapContainer.current = el)} style={mapStyle}/>
   )
 }
